d3d0b889f0098da52319488373aa683727f62f80
Roland <roli.vincze@gmail.com>
2024-01-21 23:45:53 +0100

    Solve exercises

--- exercise/logging/src/lib.rs
+++ exercise/logging/src/lib.rs
@@ -6,6 +6,8 @@
 //
 // Hint: You need to update Cargo.toml to add the `log` dependency, first.
 
+use log::{debug, error, info, trace, warn};
+
 #[derive(Debug)]
 pub struct Frog {
     energy: u8,
@@ -15,18 +17,22 @@ pub struct Frog {
 impl Frog {
     pub fn new() -> Self {
         // 2. Use debug!() to log "A new Frog has been created"
+        debug!(target: "new()", "A new Frog is being created");
         Default::default()
     }
     pub fn hop(&mut self) {
         self.energy -= 1;
+        info!(target: "hop()", "The Frog hopped, {} energy left", self.energy);
         // 3. Use info!() to log that a Frog hopped, and how much energy is left
         if self.energy == 0 {
+            warn!(target: "hop()", "Frog will go to sleep since its energy is 0");
             // 4. Use warn!() to warn that the frog will go to sleep since he ran out of energy
             self.sleep();
         }
     }
     pub fn sleep(&mut self) {
         if self.sleeping {
+            error!(target: "sleep()", "Frog is already asleep!")
             // 5. Use error!() to log a (non-fatal) error stating that the Frog is already asleep
         } else {
             self.sleeping = true;
@@ -37,9 +43,11 @@ impl Frog {
 impl Default for Frog {
     fn default() -> Self {
         // 6. Use trace!() to log that a default value was generated, with the debug representation
-        Frog {
+        let default_frog = Frog {
             energy: 5,
             sleeping: false,
-        }
+        };
+        trace!(target: "default()", "Default value was generated: {:?}", default_frog);
+        default_frog
     }
 }
